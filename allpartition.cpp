acf30be9e9cce7f5819333a7c5d0bd28190c1b0c
Anurag Maurya <anuragmauryag@gmail.com>
2018-04-30 01:40:32 +0530

    code for generating partitions of array

---
+++ allpartition.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(string t)
+{
+	cout<<t<<"\n";
+}
+
+void partition(int *a,int n,string t="",int pos=0)
+{
+	if(pos==n)//if a partition is ready print it
+	{
+		print(t);
+		return;
+	}
+    for(int i=pos;i<n;i++)//recurring at every position of array
+    {
+    	string p;
+    	for(int k=pos;k<=i;k++)//including element in partition starting at current position
+    	{
+    		p+=to_string(a[k]);
+    		p+=" ";
+    	}
+    	partition(a,n,t+"("+p+")",i+1);
+    }
+}
+
+signed main()
+{
+	int *a,n;
+	cout<<"n?"<<"\n";
+	cin>>n;
+	a=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		  cin>>a[i];
+	}
+  for(int i=0;i<n;i++)
+  {
+    	cout<<a[i]<<" ";
+  }
+  cout<<"\n";
+  partition(a,n);
+  return 0;
+} 
